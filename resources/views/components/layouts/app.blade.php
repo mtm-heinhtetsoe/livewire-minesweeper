9d7c9f90f5b450e560b6c8df637035ec9d1f672f
mtm-heinhtetsoe <heinhtetsoe@metateammyanmar.com>
2025-03-21 16:38:58 +0630

    feat(layouts): add word of the day stylesheet to app layout
    Include the 'wordoftheday.css' stylesheet in the app layout to support the new Word of the Day feature

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{ asset('minesweeper.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('navigation.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('wordoftheday.css') }}">
 
 
         <title>{{ $title ?? 'Minesweeper' }}</title>
