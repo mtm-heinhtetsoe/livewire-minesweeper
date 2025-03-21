58e493f5cc54b9c4badc1c0f80b5609e0a91eaea
mtm-heinhtetsoe <heinhtetsoe@metateammyanmar.com>
2025-03-21 16:16:33 +0630

    feat(layout): add navigation styles and include navigation component
    Include a new CSS file for navigation styling and add the navigation component to the app layout to enhance user navigation experience.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,10 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{ asset('minesweeper.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('navigation.css') }}">
+
 
         <title>{{ $title ?? 'Minesweeper' }}</title>
     </head>
     <body>
+        @include('navigation')
         {{ $slot }}
     </body>
 </html>
