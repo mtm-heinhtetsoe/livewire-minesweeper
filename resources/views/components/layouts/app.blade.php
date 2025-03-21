ad40f869c3dac1fe2c3efa9edf6d537191c65535
mtm-heinhtetsoe <heinhtetsoe@metateammyanmar.com>
2025-03-21 16:28:46 +0630

    added navigation

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,7 +10,16 @@
         <title>{{ $title ?? 'Minesweeper' }}</title>
     </head>
     <body>
-        @include('navigation')
+        <nav class="nav-container">
+            <div class="nav-content">
+                <div class="nav-brand">Word Games</div>
+                <div class="nav-links">
+                    <a href="/" class="nav-link">Home</a>
+                    <a href="/word-of-the-day" class="nav-link">Word of the Day</a>
+                    <a href="/minesweeper" class="nav-link">Minesweeper</a>
+                </div>
+            </div>
+        </nav>
         {{ $slot }}
     </body>
 </html>
